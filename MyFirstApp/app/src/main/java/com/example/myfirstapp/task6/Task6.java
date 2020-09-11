0bec3607002418d4bdacfc8dbde0f2f6f56bbad7
camargodev <camargo.dev@gmail.com>
2020-09-11 19:24:51 -0300

    Changed sensor on Ex 5

--- MyFirstApp/app/src/main/java/com/example/myfirstapp/task6/Task6.java
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/task6/Task6.java
@@ -1,13 +1,18 @@
 package com.example.myfirstapp.task6;
 
+import android.Manifest;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.example.myfirstapp.MainActivity;
 import com.example.myfirstapp.R;
 
 public class Task6 extends AppCompatActivity {
@@ -21,19 +26,22 @@ public class Task6 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_6);
 
-        result = (TextView) findViewById(R.id.result);
-        operand1 = (EditText) findViewById(R.id.operand1);
-        operand2 = (EditText) findViewById(R.id.operand2);
+        Button getGPSBtn = (Button) findViewById(R.id.gpstatus);
+        ActivityCompat.requestPermissions(Task6.this, new
+                String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+        getGPSBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GpsTracker gps = new GpsTracker(getApplicationContext());
+                Location location = gps.getLocation();
+                if(location != null) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+                    Toast.makeText(getApplicationContext(),
+                            "LAT: " + latitude + "\nLONG: " + longitude, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
-    public void sumValues(View view) {
-        try {
-            Integer op1 = Integer.parseInt(operand1.getText().toString());
-            Integer op2 = Integer.parseInt(operand2.getText().toString());
-            Integer res = op1 + op2;
-            result.setText(res.toString());
-        } catch (NumberFormatException e) {
-            Toast.makeText(this, "Invalid number", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
