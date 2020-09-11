0a2ec4c65a5205155cf32cb091446631eb6273cf
camargodev <camargo.dev@gmail.com>
2020-09-11 17:46:12 -0300

    Project + Exercise 1

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myfirstapp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+
+    private TextView result;
+    private EditText operand1;
+    private EditText operand2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        result = (TextView) findViewById(R.id.result);
+        operand1 = (EditText) findViewById(R.id.operand1);
+        operand2 = (EditText) findViewById(R.id.operand2);
+    }
+
+    public void sumValues(View view) {
+        try {
+            Integer op1 = Integer.parseInt(operand1.getText().toString());
+            Integer op2 = Integer.parseInt(operand2.getText().toString());
+            Integer res = op1 + op2;
+            result.setText(res.toString());
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, "Invalid number", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
